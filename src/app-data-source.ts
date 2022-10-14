baa3de28dc3a4185b562b64e2de774d6fe1fa597
xnatsuk <xnatsuk@gmail.com>
2022-10-14 17:20:36 -0300

    :card_file_box: Update data-source port

--- src/app-data-source.ts
+++ src/app-data-source.ts
@@ -3,11 +3,11 @@ import { DataSource } from "typeorm";
 export const myDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST,
-  port: 5432,
+  port: parseInt(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   entities: ["src/entity/*.ts"],
-  logging: false,
+  logging: true,
   synchronize: false,
 });
